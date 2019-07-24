9da060411a6f4cbe4741187db5e1cb7e30ed5c6f
Erica Tollefson <etollefson@cnm.edu>
2019-07-23 19:59:23 -0600

    updated name of Foo.php to Author.php and added ext-pdo line in composer.json

--- php/lib/foo.php
+++ php/lib/foo.php
@@ -1,6 +1,6 @@
 <?php
 namespace Etollefson\ObjectOriented;
-require_once('../Classes/Foo.php');
+require_once('../Classes/Author.php');
 
 
 $myAuthor = new author('10554ef5-6a07-42ae-adbc-1aa63d56cc15', 'https://www.uuidgenerator.net/', null, 'dude@email.net', '$argon2i$v=19$m=1024,t=2,p=2$QWVYSXM1Tm92ekpBTlpYdg$oqc0fn/lGJ06bENiDk+bm2EWub5jOOuuqPBlqXl+f+A', 'myUsername');
